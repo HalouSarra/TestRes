bacdeab830b985e06d30d5b5f517e7e6b937f7f2
sarra halou <halousarra@gmail.com>
2023-06-05 18:56:32 +0100

    init 3

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -26,54 +26,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        date=findViewById(R.id.dateDebutRes);
-        hour=findViewById(R.id.heurDebutRes);
 
-        timeBooking=findViewById(R.id.heurDebutRes);
+                date=findViewById(R.id.dateDebutRes);
+                hour=findViewById(R.id.heurDebutRes);
 
-        Calendar calendar =Calendar.getInstance();
-        final int year =calendar.get(Calendar.YEAR);
-        final int month = calendar.get(Calendar.MONTH);
-        final int day =calendar.get(Calendar.DAY_OF_MONTH);
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatePickerDialog datePickerDialog=new DatePickerDialog(
-                        MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
-                        ,setListener,year, month,day);
-                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                datePickerDialog.show();
-            }
-        });
+                timeBooking=findViewById(R.id.heurDebutRes);
 
-        setListener =new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month =month+ 1;
-                String datee=day+"/"+ month +"/"+year;
-                date.setText(datee);
-            }
-        };
-
-
-        timeBooking.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Calendar calendar=Calendar.getInstance();
-                mHour=calendar.get(Calendar.HOUR);
-                mMinute=calendar.get(Calendar.MINUTE);
-
-                TimePickerDialog timePickerDialog=new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                Calendar calendar =Calendar.getInstance();
+                final int year =calendar.get(Calendar.YEAR);
+                final int month = calendar.get(Calendar.MONTH);
+                final int day =calendar.get(Calendar.DAY_OF_MONTH);
+                date.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onTimeSet(TimePicker timePicker, int h, int m) {
-                        timeBooking.setText(h+":"+m);
+                    public void onClick(View view) {
+                        DatePickerDialog datePickerDialog=new DatePickerDialog(
+                                MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                                ,setListener,year, month,day);
+                        datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        datePickerDialog.show();
                     }
-                },mHour,mMinute,true);
-                timePickerDialog.show();
+                });
 
-            }
-        });
+                setListener =new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month =month+ 1;
+                        String datee=day+"/"+ month +"/"+year;
+                    }
+                };
 
+                date.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        DatePickerDialog datePickerDialog=new DatePickerDialog(
+                                MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                                m=month+1;
+                                String datte=d+"/"+m+"/"+y;
+                                date.setText(datte);
+                            }
+                        },year,month,day);
+                        datePickerDialog.show();
+                    }
+                });
+
+
+                timeBooking.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final Calendar calendar=Calendar.getInstance();
+                        mHour=calendar.get(Calendar.HOUR);
+                        mMinute=calendar.get(Calendar.MINUTE);
+
+                        TimePickerDialog timePickerDialog=new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int h, int m) {
+                                timeBooking.setText(h+":"+m);
+                            }
+                        },mHour,mMinute,true);
+                        timePickerDialog.show();
+
+                    }
+                });
     }
 
 }
